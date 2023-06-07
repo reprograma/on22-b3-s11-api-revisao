f91bd0e37657b81dffefd6860f138bcf0345058b
Jamile <milli.santana2017@gmail.com>
2023-06-06 21:43:19 -0300

    feat: Adiciona rota get para buscar uma serie pelo id

--- exercicios/para-casa/series/src/controller/seriesController.js
+++ exercicios/para-casa/series/src/controller/seriesController.js
@@ -4,6 +4,13 @@ const getAllSeries = (_req, res) => {
   res.status(200).send(seriesJson);
 }
 
+const getSeriesbyid = (req, res) => {
+  const id = req.params.id;
+  const requestSerie = seriesJson.filter(serie => serie.id == id);
+  res.status(200).json(requestSerie);
+}
+
 module.exports = {
-  getAllSeries
+  getAllSeries,
+  getSeriesbyid
 }
--- exercicios/para-casa/series/src/routes/routes.js
+++ exercicios/para-casa/series/src/routes/routes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const seriesController = require("../controller/seriesController");
 
 router.get("/", seriesController.getAllSeries);
+router.get("/series/:id", seriesController.getSeriesbyid);
 
 module.exports = router;
